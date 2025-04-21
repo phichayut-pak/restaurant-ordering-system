73e15e17bec9f52a23f0d38daa102308b52ad23d
AgilitiesZ <pakkung1245@gmail.com>
2025-04-21 23:16:45 +0700

    updated OrderWriter

--- OrderWriter.java
+++ OrderWriter.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 
 public class OrderWriter {
     public static void writeReceipt(Order order, String filename) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            writer.write("Recept:\n");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true))) {
+            writer.write("Receipt:\n");
             for (MenuItem item : order.getItems()) {
                 writer.write(item.getName() + " - " + item.getPrice() + " baht\n");
             }
 
             writer.write("Status: " + order.getStatus() + "\n");
+            writer.write("\n"); // Add a line break between orders if needed
         } catch (IOException e) {
             e.printStackTrace();
         }
